475424500b4ca022d30e7dc4cf4a86f87d9d84a8
alla2297 <alla2297@edu.easj.dk>
2021-09-06 14:27:48 +0200

    version 1,2

--- src/lommeregner.java
+++ src/lommeregner.java
@@ -4,30 +4,32 @@ public class lommeregner {
     public static void main(String[] args) {
         System.out.println("Brug lomme regner ");
         Scanner scanner = new Scanner(System.in);
+        boolean exit = false;
+        while (!exit) {
+                System.out.println("Indtast et tal: ");
+                double x = scanner.nextDouble();
+
+                System.out.println("Indtast endnu et tal: ");
+                double y = scanner.nextDouble();
 
 
-            System.out.println("Indtast et tal: ");
-            double x = scanner.nextDouble();
+                System.out.println("Hvordan vil du regne med det nye tal? Skriv +, -, / eller *");
+                String symbol = scanner.next();
 
-            System.out.println("Indtast endnu et tal: ");
-            double y = scanner.nextDouble();
+                if (symbol.equals("+")) {
+                    System.out.println("result = " + (x + y));          // here calculate if  the symbol is +
+                }
+                if (symbol.equals("-")) {
+                    System.out.println("result = " + (x - y));          // here calculate if  the symbol is -
 
+                }
+                if (symbol.equals("*")) {
+                    System.out.println("result = " + (x * y));          // here calculate if  the symbol is *
+                }
+                if (symbol.equals("/")) {
+                    System.out.println("result = " + (x / y));          // here calculate if  the symbol is /
+                }
 
-            System.out.println("Hvordan vil du regne med det nye tal? Skriv +, -, / eller *");
-            String symbol = scanner.next();
-
-            if (symbol.equals("+")) {
-                System.out.println("result = " + (x + y));
-            }
-            if (symbol.equals("-")) {
-                System.out.println("result = " + (x - y));
-
-            }
-            if (symbol.equals("*")) {
-                System.out.println("result = " + (x * y));
-            }
-            if (symbol.equals("+")) {
-                System.out.println("result = " + (x + y));
             }
 
     }
